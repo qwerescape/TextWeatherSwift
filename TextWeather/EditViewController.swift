d511c82bb4567eb5c86455dcf3c9f485f3b7d77c
Chao Qu <chaoqu@gmail.com>
2014-07-28 00:29:57 -0400

    holy fuck, i don't know why, but this hack works

--- TextWeather/EditViewController.swift
+++ TextWeather/EditViewController.swift
@@ -16,21 +16,34 @@ class EditViewController: UIViewController, UIScrollViewDelegate {
         self.navigationController.setNavigationBarHidden(false, animated: true)
 //        self.navigationController.navigationItem.backBarButtonItem.title = "DONE"
         super.viewWillAppear(animated)
+        self.view.setNeedsLayout()
+        self.view.layoutIfNeeded()
     }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
     override func viewWillLayoutSubviews() {
-        let carouselRect = weatherTextCarousel.frame
-        let alternativeTextFrame: CGRect = CGRect(origin: CGPoint(x: 10, y: 10), size: CGSize(width: carouselRect.size.width-20, height: carouselRect.size.height-20))
+        let carouselRect = weatherTextCarousel.bounds
+        NSLog("WTF man %f, %f", carouselRect.size.height, carouselRect.size.width)
+        
+        NSLog("WTF man2 %f, %f", carouselRect.height, carouselRect.width)
+        let alternativeTextFrame: CGRect = CGRect(origin: CGPoint(x: 10, y: 0), size: CGSize(width: carouselRect.size.width-20, height: carouselRect.size.height))
         //find the subview
-        if let textView = weatherTextCarousel.subviews[0] as? UITextView {
-            textView.frame = alternativeTextFrame
+        for subView in weatherTextCarousel.subviews {
+            if let textView = subView as? UITextView {
+                textView.frame = alternativeTextFrame
+            }
         }
+//        (weatherTextCarousel.subviews[1] as UIView).frame = alternativeTextFrame
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         //add some text views
         let alternativeText: UITextView = UITextView()
         alternativeText.editable = false
-        alternativeText.text = "very long ass mother fucking shit stuff that will for sure scroll you son of a bitch, piece of shit i know you won't work the first timevery long ass mother fucking shit stuff that will for sure scroll you son of a bitch, piece of shit i know you won't work the first timevery long ass mother fucking shit stuff that will for sure scroll you son of a bitch, piece of shit i know you won't work the first timevery long ass mother fucking shit stuff that will for sure scroll you son of a bitch, piece of shit i know you won't work the first timevery long ass mother fucking shit stuff that will for sure scroll you son of a bitch, piece of shit i know you won't work the first timevery long ass mother fucking shit stuff that will for sure scroll you son of a bitch, piece of shit i know you won't work the first timevery long ass mother fucking shit stuff that will for sure scroll you son of a bitch, piece of shit i know you won't work the first timevery long ass mother fucking shit stuff that will for sure scroll you son of a bitch, piece of shit i know you won't work the first timevery long ass mother fucking shit stuff that will for sure scroll you son of a bitch, piece of shit i know you won't work the first time"
+        alternativeText.text = "very long ass mother fucking shit stuff that will for sure scroll you son of a bitch, piece of shit i know you won't work the first timevery long ass mother fucking shit stuff that will for sure scroll you son of a bitch, piece of shit i know you won't work the first timevery long ass mother fucking shit stuff that will for sure scroll you son of a bitch, piece of shit i know you won't work the first timevery long ass mother fucking shit stuff that will for sure scroll you son of a bitch, piece of shit i know you won't work the first timevery long ass mother fucking shit stuff that will for sure scroll you son of a bitch, piece of shit i know you won't work the first timevery long ass mother fucking shit stuff that will for sure scroll you son of a bitch, piece of shit i know you won't work the first timevery long ass mother fucking shit stuff that will for sure scroll you son of a bitch, piece of shit i know you won't work the first timevery long ass mother fucking shit stuff that will for sure scroll you son of a bitch, piece of shit i know you won't work the first timevery long ass mother fucking shit stuff that will for sure scroll you son of a bitch, piece of shit i know you won't work the first timeure scroll you son of a bitch, piece of shit i know you won't work the first timevery long ass mother fucking shit stuff that will for sure scroll you son of a bitch, piece of shit i know you won't work the first timevery long ass mother fucking shit stuff that will for sure scroll you son of a bitch, piece of shit i know you won't work the first timevery long ass mother fucking shit stuff that will for sure scroll you son of a bitch, piece of shit i know you won't work the first timevery long ass mother fucking shit stuff that will for sure scroll you son of a bitch, piece of shit i know you won't work the first timevery long ass mother fucking shit stuff that will for sure scroll you son of a bitch, piece of shit i know you won't work the first timeure scroll you son of a bitch, piece of shit i know you won't work the first timevery long ass mother fucking shit stuff that will for sure scroll you son of a bitch, piece of shit i know you won't work the first timevery long ass mother fucking shit stuff that will for sure scroll you son of a bitch, piece of shit i know you won't work the first timevery long ass mother fucking shit stuff that will for sure scroll you son of a bitch, piece of shit i know you won't work the first timevery long ass mother fucking shit stuff that will for sure scroll you son of a bitch, piece of shit i know you won't work the first timevery long ass mother fucking shit stuff that will for sure scroll you son of a bitch, piece of shit i know you won't work the first time"
         weatherTextCarousel.addSubview(alternativeText)
     }
     
